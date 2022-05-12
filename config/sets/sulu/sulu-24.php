446734737970b52dd9e83e98fe502c085a8828ec
Alexander Schranz <alexander@sulu.io>
2022-05-12 17:31:30 +0200

    Add first rule for sulu rector (#1)

--- config/sets/sulu/sulu-24.php
+++ config/sets/sulu/sulu-24.php
@@ -3,7 +3,27 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
 
 return static function (RectorConfig $rectorConfig): void {
-    // TODO configure upgrades
+    $rectorConfig
+        ->ruleWithConfiguration(RenameMethodRector::class, [
+            // @see https://github.com/sulu/sulu/pull/6071
+            new MethodCallRename(
+                'Sulu\Component\Localization\Localization',
+                'setXDefault',
+                'setDefault',
+            ),
+            new MethodCallRename(
+                'Sulu\Component\Localization\Localization',
+                'getXDefault',
+                'getDefault',
+            ),
+            new MethodCallRename(
+                'Sulu\Component\Localization\Localization',
+                'isXDefault',
+                'isDefault',
+            ),
+        ]);
 };
---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $services = $rectorConfig->services();
+    $rectorConfig->importNames();
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+
+    $rectorConfig->parallel();
+
+    // for testing
+    $rectorConfig->import(__DIR__ . '/config/config.php');
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::NAMING,
+    ]);
+};
