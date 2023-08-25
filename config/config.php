f351c01f23b1102ede340229f0d3a779d3180535
Alexander Schranz <alexander@sulu.io>
2023-08-25 12:43:11 +0200

    Add support for rector 0.18 (#21)

--- config/config.php
+++ config/config.php
@@ -3,21 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\NonPhpFile\Rector\RenameClassNonPhpRector;
-use Symplify\SmartFileSystem\SmartFileSystem;
 
 return static function (RectorConfig $rectorConfig): void {
-    $services = $rectorConfig->services();
-
-    $services->defaults()
-        ->public()
-        ->autowire()
-        ->autoconfigure();
-
-    $services->load('Sulu\\Rector\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/{Rector,ValueObject}']);
-
-    $rectorConfig->rule(RenameClassNonPhpRector::class);
-
-    $services->set(SmartFileSystem::class);
+    // $rectorConfig->singleton(ServiceType::class);
 };
