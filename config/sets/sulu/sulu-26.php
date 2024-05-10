50b14bc6fda467e1815f6a7dcc19833a7214741c
Alexander Schranz <alexander@sulu.io>
2024-05-10 18:31:58 +0200

    Update code style

--- config/sets/sulu/sulu-26.php
+++ config/sets/sulu/sulu-26.php
@@ -2,19 +2,7 @@
 
 declare(strict_types=1);
 
-use PHPStan\Type\ArrayType;
-use PHPStan\Type\BooleanType;
-use PHPStan\Type\FloatType;
-use PHPStan\Type\IntegerType;
-use PHPStan\Type\IterableType;
-use PHPStan\Type\MixedType;
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\StringType;
 use Rector\Config\RectorConfig;
-use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
-use Rector\Renaming\ValueObject\MethodCallRename;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
-use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 
 return static function (RectorConfig $rectorConfig): void {
 };
