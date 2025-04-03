5f0c3daa2675011c2cf1a51007e649706a441955
mamazu <14860264+mamazu@users.noreply.github.com>
2025-04-04 01:13:04 +0200

    Including the deprecated `ListBuilderInterface`
    See https://github.com/sulu/sulu/pull/7752

--- config/sets/sulu/sulu-30.php
+++ config/sets/sulu/sulu-30.php
@@ -19,6 +19,9 @@ return static function (RectorConfig $rectorConfig): void {
             new MethodCallRename('Sulu\Bundle\CategoryBundle\Entity\Category', 'addChildren', 'addChild'),
             new MethodCallRename('Sulu\Bundle\CategoryBundle\Entity\Category', 'removeChildren', 'removeChild'),
             new MethodCallRename('Sulu\Bundle\AdminBundle\Admin\View\FormOverlayListViewBuilder', 'setRequestParameters', 'addRequestParameters'),
+            new MethodCallRename('Sulu\Component\Rest\ListBuilder\ListBuilderInterface', 'setFields', 'setSelectFields'),
+            new MethodCallRename('Sulu\Component\Rest\ListBuilder\ListBuilderInterface', 'addField', 'addSelectField'),
+            new MethodCallRename('Sulu\Component\Rest\ListBuilder\ListBuilderInterface', 'hasField', 'hasSelectField'),
         ],
     );
 
