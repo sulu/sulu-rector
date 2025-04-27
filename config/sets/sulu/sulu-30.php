21da203cac10267351ffcdbdd3115f8234fa464e
mamazu <14860264+mamazu@users.noreply.github.com>
2025-04-27 15:27:34 +0200

    Also adding class remappings after the bundles that have been moved to core

--- config/sets/sulu/sulu-30.php
+++ config/sets/sulu/sulu-30.php
@@ -3,46 +3,29 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
-use Rector\Renaming\ValueObject\MethodCallRename;
-use Sulu\Rector\Rector\ListBuilderInterfaceRector;
-use Sulu\Rector\Rector\PaginatedRepresentationRector;
-use Sulu\Rector\Rector\RequestParameterTraitRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->ruleWithConfiguration(
-        RenameMethodRector::class,
-        [
-            // @see Removing old Category functions https://github.com/sulu/sulu/pull/7572
-            new MethodCallRename(
-                'Sulu\Bundle\CategoryBundle\Entity\CategoryRepository',
-                'findByCategoryIds',
-                'findByCategoriesIds',
-            ),
-            new MethodCallRename('Sulu\Bundle\CategoryBundle\Entity\Category', 'addChildren', 'addChild'),
-            new MethodCallRename('Sulu\Bundle\CategoryBundle\Entity\Category', 'removeChildren', 'removeChild'),
+    $classRenames = [
+        'Sulu\Bundle\RouteBundle\Model\RoutableInterface' => 'Sulu\Bundle\ContentBundle\Content\Domain\Model\RoutableTrait',
+    ];
 
-            new MethodCallRename('Sulu\Bundle\AdminBundle\Admin\View\FormOverlayListViewBuilder', 'setRequestParameters', 'addRequestParameters'),
+    $modelClasses = [
+        'ContentRichEntityInterface',
+        'DimensionContentInterface',
+        'DimensionContentTrait',
+        'RoutableInterface',
+        'TemplateInterface',
+        'TemplateTrait',
+        'WorkflowInterface',
+        'WorkflowTrait',
+    ];
 
-            // @see Removing old ListBuilderInterface functions https://github.com/sulu/sulu/pull/7752
-            new MethodCallRename('Sulu\Component\Rest\ListBuilder\ListBuilderInterface', 'setFields', 'setSelectFields'),
-            new MethodCallRename('Sulu\Component\Rest\ListBuilder\ListBuilderInterface', 'addField', 'addSelectField'),
-            new MethodCallRename('Sulu\Component\Rest\ListBuilder\ListBuilderInterface', 'hasField', 'hasSelectField'),
+    // Content Bundle
+    foreach ($modelClasses as $modelClass) {
+        $classRenames['Sulu\\Bundle\\ContentBundle\\Content\\Domain\\Model\\' . $modelClass] = 'Sulu\\Content\\Domain\\Model\\' . $modelClass;
+    }
 
-            // @see Deprecating Localization https://github.com/sulu/sulu/pull/7053
-            new MethodCallRename('Sulu\Component\Webspace\Portal', 'getXDefaultLocalization', 'getDefaultLocalization'),
-            new MethodCallRename('Sulu\Component\Webspace\Portal', 'setXDefaultLocalization', 'setDefaultLocalization'),
-            new MethodCallRename('Sulu\Component\Localization\Localization', 'isXDefault', 'isDefault'),
-            new MethodCallRename('Sulu\Component\Security\Event\PermissionUpdateEvent', 'getSecurityIdentity', 'getPermissions'),
-        ],
-    );
-
-    // @see Replacing the RequestParameterTrait: https://github.com/sulu/sulu/pull/7815
-    $rectorConfig->rule(RequestParameterTraitRector::class);
-
-    // @see Manually fixing whereNot -> where: https://github.com/sulu/sulu/pull/7752
-    $rectorConfig->rule(ListBuilderInterfaceRector::class);
-
-    // @see ListRepresentation -> PaginatedRepresentation https://github.com/sulu/sulu/pull/7740
-    $rectorConfig->rule(PaginatedRepresentationRector::class);
+    // TODO: Add all the renames for bundles here:
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, $classRenames);
 };
